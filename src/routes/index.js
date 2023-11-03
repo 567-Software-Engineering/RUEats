3bc8c74cf416a4639109aa684edf463d1e3b8e70
Keshav <keshavshivkumar8@gmail.com>
2023-11-03 00:20:57 -0400

    Features registered user: add review, and restaurant: order history, respond to feedback,  add/delete menu items, view insights (#23)
    * added apis to see order history, reply to review
    
    * added routes to index
    
    * added bases for insights, adding review
    
    * added delete menu, minor edits to prior stuff
    
    * added delete menu, updated prior apis with auth
    
    * updated post request call
    
    * allows only users who ordered to leave a review

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,69 +3,76 @@ const controllerClass = require('./../controllers/Controller');
 const validateUserData = require('./../validations/validateUserData');
 const validateLocationData = require('./../validations/validateLocationData');
 const validateSetLocationData = require('./../validations/validateSetLocationData');
+const validatePostRequests = require('./../validations/validatePostRequests');
 
 const validateDeliveryAssociate = require('../validations/validateDeliveryAssociate');
 const controller = new controllerClass();
 
 const routes = {
     "/" : {
-        GET:(_req, res) =>{
-            response(res,{data : "Welcome to RUEats!" });
-        }
+      GET:(_req, res) =>{
+        response(res,{data : "Welcome to RUEats!" });
+      }
     },
     "/users/login":{
-        POST: (req, res) => {
-            validateUserData(req, res, controller.loginUser);
-          },
+      POST: (req, res) => {
+          validateUserData(req, res, controller.loginUser);
+        },
     },
     "/users/register": {
-        POST: (req, res) => {
-          validateUserData(req, res, controller.createUser);
-        },
+      POST: (req, res) => {
+        validateUserData(req, res, controller.createUser);
       },
-    //   "/users/:id": {
-    //     GET: controller.getUserById,
-    //     DELETE: controller.deleteUserById,
-    //     PUT: (req, res) => {
-    //       validateUserData(req, res, userController.updateUser);
-    //     },
+    },
+
+    // "/users/:id": {
+    //   GET: controller.getUserById,
+    //   DELETE: controller.deleteUserById,
+    //   PUT: (req, res) => {
+    //     validateUserData(req, res, userController.updateUser);
     //   },
+    // },
+
     "/users/:userID/orders/:orderID" :{
-        GET : controller.getUserOrder
+      GET : controller.getUserOrder
     },
     "/restaurants": {
-        GET: controller.getAllRestaurants,
-      },
+      GET: controller.getAllRestaurants,
+    },
       
     "/get-location":{
       POST: (req, res) => {
-        validateLocationData(req, res, controller.getLatitudeLongitude);
+          validateLocationData(req, res, controller.getLatitudeLongitude);
       },
     },
 
     "/delivery-associate/login":{
       POST: (req, res) => {
           validateUserData(req, res, controller.loginAssociate);
-        },
+      },
     },
+
     "/delivery-associate/register": {
         POST: (req, res) => {
           validateUserData(req, res, controller.createAssociate);
-        },
       },
-    "/restaurants/login": {
-        POST: (req, res) => {
-            validateUserData(req, res, controller.loginRestaurant);
-        },
     },
+
+      "/restaurants/login": {
+        POST: (req, res) => {
+          validateUserData(req, res, controller.loginRestaurant);
+      },
+    },
+
     "/restaurants/register": {
         POST: (req, res) => {
-            validateUserData(req, res, controller.createRestaurant);
-        },
+          validateUserData(req, res, controller.createRestaurant);
+      },
     },
+  
     "/get-notifications/:restaurant_id/notifications": {
-      GET: controller.getRestaurantNotifications
-    },
+          GET : controller.getRestaurantNotifications
+      },
     "/set-location":{
       POST: (req, res) => {
         validateSetLocationData(req, res, controller.setLocationDeliveryAssociates);
@@ -75,7 +82,36 @@ const routes = {
       POST: (req, res) => {
         validateDeliveryAssociate(req, res, controller.getClosestAssociate);
       },
+    },    "/order-history/:restaurantID/orders" : {
+        GET : controller.getOrdersForRestaurant
     },
+
+    "/respond-feedback/:restaurantID" : {
+        PUT : (req, res) => {
+          validatePostRequests(req, res, controller.respondToFeedback);
+        },
+    },
+
+    "/add-menu/:restaurantID/menu": {
+        POST : (req, res) => {
+          validatePostRequests(req, res, controller.addMenuItem);
+        },
+    },
+
+    "/restaurants/:restaurantID/reviews" : {
+        POST : (req, res) => {
+          validatePostRequests(req, res, controller.postRestaurantReview);
+        },
+    },
+
+    "/view-insights/:restaurantID/insights": {
+        GET : controller.getRestaurantInsights
+    },
+
+    "/delete-item/:restaurantID/:itemID": {
+        DELETE : controller.deleteMenuItem
+    },
+
     notFound : (_req, res) => {
         response(res, {status : 404, data : "Requested URL not found"});
     }
