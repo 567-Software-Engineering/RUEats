f5d08da46c350764927591597889fbbca1fbf19c
Shashwath-kumar <shashwath457@gmail.com>
2023-11-02 22:14:27 -0400

    active orders should be only status = 0,1

--- src/db/RUEatsRepository.js
+++ src/db/RUEatsRepository.js
@@ -175,7 +175,7 @@ module.exports = class RUEatsRepository {
   
   getOrdersByRestaurantID(restaurantID) {
     return new Promise((resolve, reject) => {
-      this.connection.query('SELECT * FROM orders WHERE restaurant_id = ? AND status IN (0,1,2)', [restaurantID], function (error, results, fields) {
+      this.connection.query('SELECT * FROM orders WHERE restaurant_id = ? AND status IN (0,1)', [restaurantID], function (error, results, fields) {
         if (error) {
           reject(error);
         } else {
