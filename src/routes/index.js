7aa785bcf6a4a7556abbf284dd7f8467b5b3d743
saurabhk98 <kamblessaurabh@gmail.com>
2023-12-05 21:21:04 -0500

    added restaurant owner email verification

--- src/routes/index.js
+++ src/routes/index.js
@@ -241,6 +241,10 @@ const routes = {
       GET: controller.updateDeliveryAssociateVerification,
     },
 
+    "/verifyRestaurant/:restaurant_id": {
+      GET: controller.updateRestaurantVerification,
+    },
+
     notFound: (_req, res) => {
         response(res, { status: 404, data: "Requested URL not found" });
 
